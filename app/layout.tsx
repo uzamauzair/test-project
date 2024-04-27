11b58747ea3ad3b807185df6f2c67bee9c6f0188
uzamauzair <usama19026@gmail.com>
2024-04-27 17:25:56 +0530

    01-Create-Next-App-Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright © - All right reserved by SoftSora</p>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
